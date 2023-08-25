604986f592bc0b6f9cac75d2b77d839d0dbb1ff6
please <abuhurairah131@gmail.com>
2023-08-25 23:29:51 +0500

    please

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
-
+import { useState,useEffect } from 'react';
+import { getNews } from '../../api/external';
 const Home = () => {
+  const [articles,setArticles]=useState([]);
+  useEffect(()=>{ 
+  // IIFE
+  (async function newApiCall(){
+let response= await getNews();
+setArticles(response);
+  }
+  )()
+
+
+
+  },[])
+  console.log(articles)
   return (
     <div>Home</div>
   )
