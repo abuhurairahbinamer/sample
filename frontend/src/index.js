f03d21817011067a3ae5a6777428038572c38c92
abuhurairah <abuhurairah131@gmail.com>
2025-03-02 14:41:08 +0500

    clone

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -28,3 +28,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// hellow i am abuhurairah for cloning
